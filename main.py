d7ec154d86d22c06349786f8fb6ce8354e84e727
马军 <majun@xiwicloud.com>
2025-07-06 13:53:30 +0800

    fix: add

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from mcp!")
+# --directory e:/test07/mcpServe run main.py
+from mcp.server.fastmcp import FastMCP
 
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool()
+def sum(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    main()
+    # mcp.run(transport="stdio")
+        mcp.run(transport="sse")
